20ad2e87b7a2013536050c844dd6197196f16d04
ViniciusTei <viniciustprates@gmail.com>
2023-09-19 08:25:28 -0300

    fix autosession dir

--- nvim/.config/nvim/lua/plugins/autosessions.lua
+++ nvim/.config/nvim/lua/plugins/autosessions.lua
@@ -2,10 +2,15 @@ require('auto-session').setup {
   log_level = vim.log.levels.ERROR,
   auto_session_suppress_dirs = { "~/", "~/Documentos", "~/Downloads", "/"},
   auto_session_use_git_branch = false,
-  auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+  auto_session_root_dir = "~/.config/nvim/sessions/",
   auto_session_enabled = true,
   auto_save_enabled = true,
   auto_restore_enabled = true,
   auto_session_enable_last_session = true,
-  auto_session_last_session_dir = vim.fn.stdpath('data').."/sessions/",
+  auto_session_last_session_dir = "~/.config/nvim/sessions/",
+  cwd_change_handling = {
+    post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+      require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+    end,
+  },
 }
