5b10efbf04bbe02e989bcefa2bc1b2fbf019949c
ViniciusTei <viniciustprates@gmail.com>
2024-01-20 07:40:10 -0300

    feat save session by git branch

--- nvim/.config/nvim/lua/plugins/autosessions.lua
+++ nvim/.config/nvim/lua/plugins/autosessions.lua
@@ -1,7 +1,7 @@
 require('auto-session').setup {
   log_level = vim.log.levels.ERROR,
   auto_session_suppress_dirs = { "~/", "~/Documents", "~/Downloads", "/" },
-  auto_session_use_git_branch = false,
+  auto_session_use_git_branch = true,
   auto_session_root_dir = "~/.config/nvim/sessions/",
   auto_session_enabled = true,
   auto_save_enabled = true,
