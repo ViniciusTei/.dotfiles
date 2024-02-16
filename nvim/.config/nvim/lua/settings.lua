bd14a0b5d91b784808d325cbc0812df60f640248
ViniciusTei <viniciustprates@gmail.com>
2024-02-16 08:36:34 -0300

    feat add search and replace vim remap

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -81,6 +81,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Remap to search and replace
+vim.keymap.set({ "v", "n" }, "<leader>sr", ":%s/", { desc = "Buffer search and replace" })
+
 vim.opt.colorcolumn = '80'
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
