81def9a7445c1122df61ecad139512c1b78d8be5
ViniciusTei <viniciustprates@gmail.com>
2024-02-16 08:37:06 -0300

    feat add more prefix descriptions

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -2,10 +2,7 @@ local wk = require('which-key')
 
 -- TODO - Add more keybinds, and organize some of the existing ones.
 wk.register({
-  g = {
-    name = "Go to"
-  },
-  o = {
-    name = "Open"
-  },
+  g = { name = "Go to" },
+  m = { name = "Format" },
+  o = { name = "Open" },
 }, { prefix = '<leader>' })
