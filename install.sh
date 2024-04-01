7a24a7e6556aa7a57e85b5b4a3f849f4c80cb527
ViniciusTei <viniciustprates@gmail.com>
2024-04-01 17:15:18 -0300

    fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# git clone https://github.com/ViniciusTei/.dotfiles.git
-# cd .dotfiles
-#
-# . ~/.scripts/setup.sh
+git clone https://github.com/ViniciusTei/.dotfiles.git
+cd .dotfiles
+
+. ~/.scripts/setup.sh
 echo "Setup complete"
