bf77ca50e1436271767df50d7bdf315966bf60b8
ViniciusTei <viniciustprates@gmail.com>
2025-01-16 07:34:22 -0300

    fix layout indicator correctm path

--- scripts/.scripts/i3status.sh
+++ scripts/.scripts/i3status.sh
@@ -3,6 +3,6 @@
 i3status -c "~/.config/i3/i3status.conf" | while :
 do
   read line
-  layout=$(~/Downloads/xkblayout-state/xkblayout-state print %s)
+  layout=$(xkblayout-state print %s)
   echo "󰌌 $layout | $line" || exit 1
 done
