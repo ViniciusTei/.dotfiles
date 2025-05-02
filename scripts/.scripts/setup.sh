5711a935722c644688f7256ff46ab44d02ed2cac
ViniciusTei <viniciustprates@gmail.com>
2025-05-02 08:49:55 -0300

    feat: change dmenu to rofi for application launcher

--- scripts/.scripts/setup.sh
+++ scripts/.scripts/setup.sh
@@ -4,7 +4,7 @@ echo "Setting up development tools..."
 
 echo "Installing dependencies..."
 sudo apt update
-sudo apt install fzf stow xclip ripgrep tmux i3 libx11-dev feh
+sudo apt install fzf stow xclip ripgrep tmux i3 libx11-dev feh rofi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
