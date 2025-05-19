37601a82766dcfbd916eb71a58c74a161b07af3b
ViniciusTei <viniciustprates@gmail.com>
2025-05-19 08:22:09 -0300

    feat: add catppuccin theme for tmux

--- scripts/.scripts/setup.sh
+++ scripts/.scripts/setup.sh
@@ -15,6 +15,9 @@ stow i3
 stow tmux 
 stow scripts
 
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
 tmux source ~/.tmux.conf
 
 # nvm install
