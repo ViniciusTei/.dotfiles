5908b892fe16c3bdfaf7501ea361ba1677a42fd9
Vinicius Teixeira <vinicius.teixeira@cbrdoc.com.br>
2026-04-14 11:35:55 -0300

    feat: add aplha ui for neovim startup configuration

--- nvim/.config/nvim/lua/vinicius/plugins/ui.lua
+++ nvim/.config/nvim/lua/vinicius/plugins/ui.lua
@@ -1,3 +1,23 @@
+local b0 = {
+  '                                                    ',
+  ' ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+  ' ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+  ' ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+  ' ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+  ' ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+  ' ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+  '                                                    ',
+}
+local b1 = {
+  '                                                       ',
+  ' ██╗   ██╗██╗███╗   ██╗██╗ ██████╗██╗██╗   ██╗███████╗ ',
+  ' ██║   ██║██║████╗  ██║██║██╔════╝██║██║   ██║██╔════╝ ',
+  ' ██║   ██║██║██╔██╗ ██║██║██║     ██║██║   ██║███████╗ ',
+  ' ╚██╗ ██╔╝██║██║╚██╗██║██║██║     ██║██║   ██║╚════██║ ',
+  '  ╚████╔╝ ██║██║ ╚████║██║╚██████╗██║╚██████╔╝███████║ ',
+  '   ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝╚═╝ ╚═════╝ ╚══════╝ ',
+  '                                                       ',
+}
 return {
   { -- Colorscheme
     'folke/tokyonight.nvim',
@@ -34,4 +54,22 @@ return {
   },
 
   { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
+      dashboard.section.header.val = b1
+      -- Menu (Shortcuts)
+      dashboard.section.buttons.val = {
+        dashboard.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
+        dashboard.button('f', '  Find file', ':Telescope find_files<CR>'),
+        dashboard.button('g', '󱈇  Grep', ':Telescope live_grep<CR>'),
+        dashboard.button('s', '  Settings', ':e $MYVIMRC<CR>'),
+        dashboard.button('q', '  Quit', ':qa<CR>'),
+      }
+      alpha.setup(dashboard.config)
+    end,
+  },
 }
