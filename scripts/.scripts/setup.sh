edd24f9908856ed1e389df6c6eb718e72c353c47
Vinicius Teixeira <viniciustprates@gmail.com>
2026-06-23 09:32:59 -0300

    Update setup.sh to include jq

--- scripts/.scripts/setup.sh
+++ scripts/.scripts/setup.sh
@@ -4,7 +4,7 @@ echo "Setting up development tools..."
 
 echo "Installing dependencies..."
 sudo apt update
-sudo apt install -y fzf stow xclip ripgrep tmux i3 libx11-dev feh rofi xdotool \
+sudo apt install -y jq fzf stow xclip ripgrep tmux i3 libx11-dev feh rofi xdotool \
     build-essential cmake cmake-data pkg-config python3 python3-sphinx \
     libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev \
     python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev \
